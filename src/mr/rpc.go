6188f949a72fa277e918692dd8c1b0b26485a7f4
GGGalaxy <33902518+wwShuang@users.noreply.github.com>
2021-09-16 09:31:04 +0800

    start mission

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -23,7 +23,13 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type Args struct {
+	X int
+}
 
+type Reply struct {
+	Y int
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
